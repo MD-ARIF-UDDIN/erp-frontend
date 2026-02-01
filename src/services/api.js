3f37c50bf33732dca3058b9c819b8adb2a334bb6
MD Arif <mdarifuddin040@gmail.com>
2026-02-02 02:02:09 +0600

    updTES

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://erp-server-ok72.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
